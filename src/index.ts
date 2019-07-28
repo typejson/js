c90c940625b771db5b0c5bbb7cdad38bf5733fc1
Nimo <nimo.jser@gmail.com>
2019-07-28 23:32:23 +0800

    remove tsdoc

--- src/index.ts
+++ src/index.ts
@@ -17,44 +17,7 @@ const TypeAliass: any = {
   "bool": "boolean",
   "boolean": "boolean"
 }
-/*
-package typejson
 
-`import "typejson"`
-
-## Index
-
-class TypeJSON
-  func parse(target: any, types: Types = {}): any
-
-Type TypeJSONClass
-Type Types
-
-
-## class TypeJSON
-
-### func parse
-
-`parse(target: any, types: Types ={}): any`
-
-Use types parse object
-
-**Example**
-
-```ts
-// Validate required
-expect(function () {
-  tjson.parse({}, {
-    "name": {
-      type: "string"
-    }
-  })
-}).toThrow(
-  new Error(`typejson: attr: "name" is requried and must be a string`)
-)
-```
-
-*/
 
 // @tsdoc.org
 class TypeJSON implements TypeJSONClass{
